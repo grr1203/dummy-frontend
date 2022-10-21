e38c509a65d07810393c663ac5011342a3c3d93b
reconlabs-hyokeun-logan <logan@reconlabs.kr>
2022-10-21 16:13:10 +0900

    jwt test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Jwt from './pages/jwt';
+import Jwt from './pages/Jwt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/jwt.js
+++ src/pages/jwt.js
@@ -2,15 +2,25 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Jwt() {
+  const [modelUid, setModelUid] = useState(0);
+  const [token, setToken] = useState();
+
   const getJWT = async () => {
     const res = await axios.get('http://localhost:4000/jwt');
+    setToken(res.data);
     console.log(res);
+  };
+
+  const sendJWT = async () => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    const res = await axios.get(`${process.env.REACT_APP_API_PATH}/model`, { params: { model_uid: modelUid } });
   };
 
   return (
     <div>
       <div>jwt test</div>
       <button onClick={getJWT}>get jwt</button>
+      <button onClick={sendJWT}>send jwt</button>
     </div>
   );
 }
