767fff684aed3b66742bfc76a19b68316b252ad6
reconlabs-hyokeun-logan <gyrms9412@naver.com>
2023-06-20 14:30:41 +0900

    paypal test

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import App from './App';
 import Jwt from './pages/Jwt';
 import MultipartUpload from './pages/MultipartUpload';
 import Pay from './pages/Pay/Pay';
-import Billing from './pages/Pay/Billing';
+import Billing from './pages/Pay/Billing'
+import PayPal from './pages/Pay/PayPal';
 import Chart from './pages/Chart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="jwt" element={<Jwt />} />
-        <Route path="/multipartupload" element={<MultipartUpload />} />
-        <Route path="/pay/*" element={<Pay />} />
-        <Route path="/billing/*" element={<Billing />} />
-        <Route path="/chart" element={<Chart />} />
-      </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="jwt" element={<Jwt />} />
+      <Route path="/multipartupload" element={<MultipartUpload />} />
+      <Route path="/pay/*" element={<Pay />} />
+      <Route path="/billing/*" element={<Billing />} />
+      <Route path="/paypal/*" element={<PayPal />} />
+      <Route path="/chart" element={<Chart />} />
+    </Routes>
+  </BrowserRouter>
 );
